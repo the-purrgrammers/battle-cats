9b1abd6adf82c46cc6de841556d90df19a1c10db
Desmond <daworam@gmail.com>
2024-05-15 10:47:17 -0500

    initial file setup cleanup / review with team

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,59 +8,8 @@ import HomePage from './Pages/HomePage';
 import GamePage from './Pages/GamePage';
 
 function App() {
-  const [message, setMessage] = useState("")
-  const [response, setResponse] = useState("")
-  const [room, setRoom] = useState('')
-  const [displayRoomNumber, setDisplayRoomNumber] = useState('')
-
-  // receives messages from the server
-  useEffect(() => {
-    socket.on("receiveMessage", (data) => {
-      setResponse(data)
-    })
-  }, [])
-
-  //assign yourself a room
-  const handleJoin = (e) => {
-    e.preventDefault()
-    socket.emit("joinRoom", room)
-    //display the number of the room you've joined
-    setDisplayRoomNumber(room)
-  }
-
-  //sends messages to the server
-  const handleMessage = (e) => {
-    e.preventDefault()
-    //send an object containing both the message and your room number
-    socket.emit("sendMessage", { message, room })
-  }
   return (
     <>
-      <form onSubmit={handleJoin}>
-        <input type="number"
-          placeholder='room number'
-          onChange={(e) => setRoom(e.target.value)}
-        />
-        <button>Join</button>
-      </form>
-      <form onSubmit={handleMessage}>
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button>send message</button>
-      </form>
-      {
-        displayRoomNumber &&
-        <p>in room {room}</p>
-      }
-      {
-        response &&
-        <p> message: {response}</p>
-
-      }
-
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/game" element={<GamePage />}></Route>
