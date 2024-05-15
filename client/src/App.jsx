021b1b9141178d94b5384e1cd2116870bed11ec7
Desmond <daworam@gmail.com>
2024-05-15 10:24:53 -0500

    initial file setup for game and home pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from 'react'
 //io import and socket connection
 import io from 'socket.io-client'
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
-const socket = io.connect(URL)
-
+const socket = io.connect(URL);
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import GamePage from './Pages/GamePage';
 
 function App() {
   const [message, setMessage] = useState("")
@@ -58,6 +60,11 @@ function App() {
         <p> message: {response}</p>
 
       }
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/game" element={<GamePage />}></Route>
+      </Routes>
     </>
   )
 }
---
+++ client/src/Pages/GamePage.jsx
@@ -0,0 +1,5 @@
+const GamePage = () => {
+  return <h1>Game Page</h1>
+}
+
+export default GamePage
