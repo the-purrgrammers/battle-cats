bc0770460e8bf6d2b8427fb3dda0a58c350e7b6c
NickRoccodev11 <nchls.rcc@gmail.com>
2024-05-15 20:17:16 -0400

    turned gameState back into an array in the route endpoint

--- server/routes/game.js
+++ server/routes/game.js
@@ -4,6 +4,7 @@ const { getGame } = require("../db/game");
 router.get("/", async (req, res) => {
   try {
     const currentGame = await getGame(1);
+    currentGame.gameState = JSON.parse(currentGame.gameState)
     res.status(200).send(currentGame);
   } catch (error) {}
 });
