ffe03e8911e9f027d4db1ae2e92dcb2a3cc307d0
Desmond <daworam@gmail.com>
2024-05-16 11:00:29 -0500

    updated styling for homepage

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return(
+  <>
+    <div className='namesDiv'>
+      <h3>Battle Cats is developed by:</h3>
+      <h3>Char</h3>
+      <h3>Desmond</h3>
+      <h3>Nick</h3>
+      <h3>Thomas</h3>
+    </div>
+  </>
+  )
+}
+
+export default Footer
