9f8d8ae2a3cc27fb13ead9ac962e5ccb7b3c1451
Desmond <daworam@gmail.com>
2024-05-22 14:36:04 -0500

    basic setup for navbar and homepage styling

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import HomeBody from "../Components/HomeBody";
 import Footer from "../Components/Footer";
+import Nav from "../Components/Nav";
 
 const HomePage = () => {
   return (
     <>
+      <Nav />
       <HomeBody />
       <Footer />
     </>
