0eeabdff4cc2dbae64aaf9f23936dd164e71983c
Desmond <daworam@gmail.com>
2024-05-24 10:25:34 -0500

    working chat in the gameroom

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import HomeBody from "../Components/HomeBody";
 import Footer from "../Components/Footer";
 import Nav from "../Components/Nav";
-import ChatBox from "../Components/ChatBox";
 
 const HomePage = () => {
   return (
