7a067af8ab581ac56f657cc9ab3cb4bdfecf40c7
Char B <charburton18@gmail.com>
2024-05-24 14:30:05 -0700

    added styling to win/lose screen

--- client/src/Components/WinLoseScreen.jsx
+++ client/src/Components/WinLoseScreen.jsx
@@ -3,12 +3,14 @@ const WinLoseScreen = ({ winnerId, playerId }) => {
 
   return (
     <>
+    <div id='win-lose-msg-cont'>
       {winnerId === playerIdNum ? (
-        <h1>you won! the felines celebrate you! you pet every cat!</h1>
+        <h1 id='winner-msg'>you won! the felines celebrate you! you pet every cat!</h1>
       ) : (
-        <h1>sorry, you lost. every kitten foiled your attempts. try again?</h1>
+        <h1 id='loser-msg'>sorry, you lost. every kitten foiled your attempts. try again?</h1>
       )
       }
+      </div>
     </>
   )
 }
