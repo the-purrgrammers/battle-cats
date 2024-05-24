51a694ef1493e2a84334b4f4dde6d89920230950
Char B <charburton18@gmail.com>
2024-05-24 14:30:56 -0700

    formatted winLoseScreen.jsx

--- client/src/Components/WinLoseScreen.jsx
+++ client/src/Components/WinLoseScreen.jsx
@@ -3,16 +3,19 @@ const WinLoseScreen = ({ winnerId, playerId }) => {
 
   return (
     <>
-    <div id='win-lose-msg-cont'>
-      {winnerId === playerIdNum ? (
-        <h1 id='winner-msg'>you won! the felines celebrate you! you pet every cat!</h1>
-      ) : (
-        <h1 id='loser-msg'>sorry, you lost. every kitten foiled your attempts. try again?</h1>
-      )
-      }
+      <div id="win-lose-msg-cont">
+        {winnerId === playerIdNum ? (
+          <h1 id="winner-msg">
+            you won! the felines celebrate you! you pet every cat!
+          </h1>
+        ) : (
+          <h1 id="loser-msg">
+            sorry, you lost. every kitten foiled your attempts. try again?
+          </h1>
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default WinLoseScreen
+export default WinLoseScreen;
