cb2b426361313772b3a772a61205fab079863e5c
Desmond <daworam@gmail.com>
2024-05-29 15:54:55 -0500

    resolved syntax issue with render

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "/auth" : "http://localhost:3000",
       "/auth/me": "http://localhost:3000",
       "/api/game" : "http://localhost:3000",
-      "/api/game/endturn" : "http://localhost:3000"
+      "/api/game/endturn" : "http://localhost:3000",
       "/api/game/endgame": "http://localhost:3000",
     }
   }
