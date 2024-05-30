00b7c2565687c74adedaee75b4d2149e4906f0c3
Nicholas Rocco <nchls.rcc@gmail.com>
2024-05-30 16:10:55 -0400

    Update HomePage.jsx
    take out useEffect

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,15 +1,8 @@
 import HomeBody from "../Components/HomeBody";
 import Footer from "../Components/Footer";
 import "../styles/homepage.css";
-import { useEffect } from "react";
 
 const HomePage = () => {
-
-//TAKE THIS OUT IF IT DOESN'T WORK!!!!
-useEffect(()=>{
-  sessionStorage.clear()
-  },[])
-  //TAKE THIS OUT IF IT DOESN'T WORK!!!!
   return (
     <>
       <div className="homeWrapper">
